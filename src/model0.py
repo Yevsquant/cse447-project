3dbf4592f236f999715a36053c6b49f7d35e7486
runying <67864475+Yevsquant@users.noreply.github.com>
2025-02-28 13:38:16 -0800

    current model 0

--- src/model0.py
+++ src/model0.py
@@ -23,7 +23,7 @@ class XLMRCP(nn.Module):
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
         self.pretrained = AutoModelForMaskedLM.from_pretrained(pretrained_model_name)
 
-    def forward_one_x(self, x, max_length: int = 1, top_k: int = 3) -> str: # not str later
+    def forward_one_x(self, x, max_length: int = 1, top_k: int = 10000) -> str: # not str later
         """
         Predict the next character based on X (input texts)
         """
@@ -35,32 +35,37 @@ class XLMRCP(nn.Module):
 
         with torch.no_grad():
             out = self.pretrained(**x)
+            ut = torch.zeros(1,60000)
 
             # Get logits for the [MASK] token
             mask_token_index = (x["input_ids"] == self.tokenizer.mask_token_id).nonzero(as_tuple=True)[1]
             logits = out.logits[0, mask_token_index, :]
 
-            # Get the top k most probable tokens
-            top_k_values, top_k_indices = torch.topk(logits, top_k, dim=-1)
-            predicted_tokens = [self.tokenizer.decode([idx.item()]) for idx in top_k_indices[0]]
+            top_k_indices, top_k_values = torch.topk(logits, top_k, dim=-1)
+            ords = [ord(self.tokenizer.decode([int(ind.item())])[0]) for ind in top_k_indices[0]]
+            for i in range(top_k):
+                if ords[i] < 60000:
+                    ut[0,ords[i]] += top_k_values[0][i].sqrt()
 
-            # Extract the first character from the top k predicted tokens
-            for token in predicted_tokens:
-                char_pred += token[0]
+            # Block
+
+            _, ut_top = torch.topk(ut, 3, dim=-1)
+            char_pred = "".join([chr(int(i.item())) for i in ut_top[0]])
 
         return char_pred
     
     def forward(self, X):
         return [self.forward_one_x(x) for x in X]
 
-"""
+
 # test if the model work
 if __name__ == '__main__':
     input_texts = ["happ",
                    "great w",
-                   "I am an a"]
+                   "I am an a",
+                   "我爱",
+                   "私は持っ"]
     model = XLMRCP()
     outputs = model(input_texts)
     print(len(outputs))
     print(outputs)
-"""
